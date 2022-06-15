135e190247fbbfd5f990638e7f06ebcbd0cf5caa
John Doe <leehy0782@naver.com>
2022-06-15 19:51:28 +0900

    init setting

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
+import * as Location from 'expo-location';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 export default function App() {
+  const [location, setLocation] = useState();
+  const [ok, setOk] = useState(true);
+  const getPermissions = async() => {
+    const permission = await Location.requestForegroundPermissionsAsync();
+    console.log(permission)
+  };
+
+  useEffect(() => {
+    getPermissions();
+  },[])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <ScrollView 
+        pagingEnabled 
+        horizontal 
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +48,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'pink'
   },
+  city: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  cityName : {
+    color: '#000',
+    fontSize: 32,
+  },
+  weather : {
+
+  },
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: 'center'
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 128
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 48
+  }
 });
